5778e15f230a097fe20172825561aee7e4b9fe52
busterxan09 <ulugnazarovhojiakbar29@gmail.com>
2026-09-21 10:47:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.types import ReactionTypeEmoji
 
-BOT_TOKEN = "8827183894:AAGzagONk1HfYdqyEmVISm_cggLr4mGjipE"
+BOT_TOKEN = "8827183894:AAFHaZShqFaRFkZU92iEwlWHTFpOHhLe0NA"
 DEFAULT_REACTIONS = ["🔥"]
 
 logging.basicConfig(level=logging.INFO)
